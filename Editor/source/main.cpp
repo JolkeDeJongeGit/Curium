c6f797702543ed714c914e80e0eee5045e03a60d
Jolke <75983607+JolkeDeJongeGit@users.noreply.github.com>
2022-10-21 16:08:13 +0200

    [Added] - Base Window

--- Editor/source/main.cpp
+++ Editor/source/main.cpp
@@ -1,8 +1,15 @@
 #include "pch.h"
+#include <platform/win32/WinWindow.h>
 
-int main(int argc, char** argv)
+int main()
 {
+	WinWindow win;
+	win.Create(1920, 1080);
+	win.SetTitle("Curium");
 
+	while (win.IsActive())
+	{
 
+	}
 	return 0;
 }
--- Engine/include/platform/Window.h
+++ Engine/include/platform/Window.h
@@ -1,6 +1,26 @@
 #pragma once
-
 class Window
 {
+public:
+	virtual void Create(int _width, int _height) = 0;
+	virtual void Update(float _deltaTime) = 0;
+	virtual void Terminate() = 0;
+	virtual void SetTitle(const std::string& _title) { m_title = _title; }
 
+	void SetWidth(const int _width) { m_width = _width; }
+	void SetHeight(const int _height) { m_height = _height; ; }
+
+	const int GetWidth() { return m_width; }
+	const int GetHeight() { return m_height; }
+
+	const std::string& GetTitle() { return m_title; }
+
+	const bool IsActive() { return m_active; }
+private:
+	int m_width = 1920;
+	int m_height = 1080;
+
+	bool m_active = true;
+
+	std::string m_title = "Curium Engine";
 };
